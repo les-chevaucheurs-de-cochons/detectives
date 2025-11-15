1270cf24c048a0ad72b2ca26df53a2fc1ee8c52d
DiegoLito03 <diegoducamp21@gmail.com>
2025-11-15 19:15:13 +0100

    Class Enquete debut

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-print('test')
-print('test')
+class Enquete:
+    def __init__(self,suspect : str,preuves : str, date : int):
+        self.suspect = suspect
+        self.preuves = preuves
+        self.date = date
+
+    def afficher(self):
+        print(f"Suspect : {self.suspect}")
+        print(f"Preuves : {self.preuves}")
+        print(f"Date : {self.date}")
+
+
+e = Enquete("pierre", "couteau", 12)
+e.afficher()
