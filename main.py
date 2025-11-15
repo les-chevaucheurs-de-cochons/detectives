cc13ba86f57492d1e712e306318a51228795e16b
DiegoLito03 <diegoducamp21@gmail.com>
2025-11-15 19:30:14 +0100

    Class Suspect debut

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+class Suspect:
+    def __init__(self,nom : str, prenom : str ,age : int, adresse : str):
+        self.nom = nom
+        self.prenom = prenom
+        self.age = age
+        self.adresse = adresse
+
+    def __str__(self):
+        return f"Suspect(nom={self.nom},prenom={self.prenom} ,age={self.age})"
+
 class Enquete:
     def __init__(self,suspect : str,preuves : str, date : int):
         self.suspect = suspect
@@ -12,3 +22,6 @@ class Enquete:
 
 e = Enquete("pierre", "couteau", 12)
 e.afficher()
+
+s = Suspect('Diego','Ducamp',21,'wavre')
+print(s)
