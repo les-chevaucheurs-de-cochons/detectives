577957fb7940e9a09b08a5888a27ec748805ef88
DiegoLito03 <diegoducamp21@gmail.com>
2025-11-15 19:46:46 +0100

    Liaison Class Suspect et Class Enquete ( ajouter_suspect)

--- main.py
+++ main.py
@@ -9,19 +9,34 @@ class Suspect:
         return f"Suspect(nom={self.nom},prenom={self.prenom} ,age={self.age})"
 
 class Enquete:
-    def __init__(self,suspect : str,preuves : str, date : int):
-        self.suspect = suspect
-        self.preuves = preuves
-        self.date = date
+    def __init__(self, titre: str, date_ouverture: int, statut: str = "ouverte"):
+        self.titre = titre
+        self.date_ouverture = date_ouverture
+        self.statut = statut
+        self.suspects: list[Suspect] = []
+
+    def ajouter_suspect(self, suspect: Suspect):
+        self.suspects.append(suspect)
 
     def afficher(self):
-        print(f"Suspect : {self.suspect}")
-        print(f"Preuves : {self.preuves}")
-        print(f"Date : {self.date}")
+        print("Suspects")
+        for s in self.suspects:
+            print(f" - {s.nom} , {s.prenom}")
+        print(f"Date d'ouverture : {self.date_ouverture}")
 
 
-e = Enquete("pierre", "couteau", 12)
-e.afficher()
+#e = Enquete("pierre", "couteau", 12)
+#e.afficher()
 
-s = Suspect('Diego','Ducamp',21,'wavre')
-print(s)
+#s = Suspect('Diego','Ducamp',21,'wavre')
+#print(s)
+
+s1 = Suspect("Pierre" , "Lee" , 19 , "Louv")
+s2 = Suspect('Diego','Ducamp',21,'wavre')
+
+e1 = Enquete("Braquage" , 2025)
+e1.ajouter_suspect(s1)
+e1.ajouter_suspect(s2)
+
+for s in e1.suspects:
+    print(s.nom)
